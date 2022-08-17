30c4232609630f5bda0d87d768d779483c1e6f3d
mfrouh <mohamedfrouh96@gmail.com>
2022-08-17 08:45:33 +0200

    edit url

--- src/config/ArjBank.php
+++ src/config/ArjBank.php
@@ -2,8 +2,8 @@
 
 return [
     'mode' => env('ARJ_MODE', 'test'), // test or live
-    'test_endpoint' => 'https://securepayments.alrajhibank.com.sa/pg/payment/hosted.htm',
-    'live_endpoint' => 'https://digitalpayments.alrajhibank.com.sa/pg/payment/hosted.htm',
+    'test_endpoint' => 'https://securepayments.alrajhibank.com.sa/pg/payment/tranportal.htm',
+    'live_endpoint' => 'https://digitalpayments.alrajhibank.com.sa/pg/payment/tranportal.htm',
     'transportal_id' => env('ARJ_TRANSPORTAL_ID'), // your transportal id
     'transportal_password' => env('ARJ_TRANSPORTAL_PASSWORD'), // your transportal password
     "resource_key" => env('ARJ_RESOURCE_KEY'), // your resource key
